c8be68637ebab7411d49ee146536354b7c7db9d4
Thomas Bakketun <thomas.bakketun@copyleft.no>
2014-03-11 13:27:24 +0100

    Timeout http request.

--- client/swf.lisp
+++ client/swf.lisp
@@ -6,19 +6,30 @@
 response. Payload is a string, headers is assoc list where the keys
 are string designators and the values are string. Returns three
 values, the result as a string, the http status code as an integer,
-and the reponse headers as an assoc list."
-  (multiple-value-bind (result-octets status-code headers)
-      (drakma:http-request uri
-                           :method :post
-                           :additional-headers (remove :content-type headers :key #'car :test #'equalp)
-                           :content-type (cdr (assoc :content-type headers :test #'equalp))
-                           :content (flex:string-to-octets payload :external-format :utf-8)
-                           :force-binary t)
-    (values (if result-octets
-                (flex:octets-to-string result-octets :external-format :utf-8)
-                "")
-            status-code
-            headers)))
+and the reponse headers as an assoc list. Returns no values if the
+request times out."
+  ;; The connection-timeout parameter to http-request doesn't work in SBCL.
+  ;; Wrapping the http request in with-timeout does work, but there are a
+  ;; risk that cleanup forms will not be run, thus leaking file descriptors.
+  ;; It's probably better to occasionally leak a file than to risk that
+  ;; that this function blocks for ever.
+  (handler-case
+      (multiple-value-bind (result-octets status-code headers)
+          (sb-ext:with-timeout 600
+            (drakma:http-request uri
+                                 :method :post
+                                 :additional-headers (remove :content-type headers :key #'car :test #'equalp)
+                                 :content-type (cdr (assoc :content-type headers :test #'equalp))
+                                 :content (flex:string-to-octets payload :external-format :utf-8)
+                                 :force-binary t))
+        (values (if result-octets
+                    (flex:octets-to-string result-octets :external-format :utf-8)
+                    "")
+                status-code
+                headers))
+    (sb-ext:timeout ()
+      (log-trace "http timeout")
+      (values))))
 
 
 (defun swf-request* (region action payload)
