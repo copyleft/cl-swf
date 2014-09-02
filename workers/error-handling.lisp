1fc411d07e28679728edd25d739b39f2a401fdfc
Thomas Bakketun <thomas.bakketun@copyleft.no>
2014-09-02 16:07:03 +0200

    report-error didn't work

--- workers/error-handling.lisp
+++ workers/error-handling.lisp
@@ -5,17 +5,15 @@
 
 
 (defun report-error (error)
-  (let ((backtrace
-	 (with-output-to-string (s) (ignore-errors
-				      (let ((sb-ext:*debug-print-variable-alist*
-					     (list (cons *print-length* 20) (cons *print-level* 6))))
-					(sb-debug:backtrace 50 s))))))
+  (ignore-errors
+    (let ((backtrace
+           (with-output-to-string (s) (let ((sb-ext:*debug-print-variable-alist*
+                                             (list (cons '*print-length* 20) (cons '*print-level* 6))))
+                                        (sb-debug:backtrace 50 s)))))
 
-    (log-error "~A"
-               (with-output-to-string (message)
-                 (flet ((heading (text)
-                          (format message "~%~%~A~%~A" text #.(make-string 100 :initial-element #\=))))
+      (log-error "~A"
+                 (with-output-to-string (message)
                    (format message "~A~%	Error of type ~S." error (type-of error))
-                   (heading "Backtrace")
+                   (terpri message)
                    (terpri message)
                    (princ backtrace message))))))
